d6fb17c024e8aa56408bc92a3a02eb8fa17d2f8c
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-01 21:41:45 +0530

    Create Binary Pattern.py

---
+++ Milestone1/5. More on Loops/Binary Pattern.py
@@ -0,0 +1,12 @@
+from math import *
+from collections import *
+from sys import *
+from os import *
+n=int(input())
+for i in range(0,n):
+    for j in range(n-i,0,-1):
+        if(i%2==0):
+            print(1,end="")
+        else:
+            print(0,end="")
+    print()
