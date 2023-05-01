bd849b8434b212cf21586432cb8a1bc8421c43bd
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-01 21:17:43 +0530

    Update Square Pattern.py

--- Milestone1/3. Patterns 1/Square Pattern.py
+++ Milestone1/3. Patterns 1/Square Pattern.py
@@ -1,15 +1,9 @@
-from math import *
-from collections import *
-from sys import *
-from os import *
 n = int(input())
-x = ord('A')
 i = 1
 while i<=n:
     j=1
-    while j<=i:
-        print(chr(x) , end="")
-        j+=1
+    while j<=n:
+        print(n , end='')
+        j=j+1
     print()
-    i+=1
-    x+=1
+    i=i+1
