e6a8b084a423f9e6d3dd515c9a8dc95ae8a6c510
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-01 21:06:31 +0530

    Create Calculator.py

---
+++ Milestone1/Conditionals and Loops/Calculator.py
@@ -0,0 +1,31 @@
+i = int(input())
+while True:
+    if(i == 1):
+        a = int(input())
+        b = int(input())
+        c = a+b
+        print(c)
+    elif(i == 2):
+        a = int(input())
+        b = int(input())
+        c = a-b
+        print(c)
+    elif(i == 3):
+        a = int(input())
+        b = int(input())
+        c = a*b
+        print(c)
+    elif(i == 4):
+        a = int(input())
+        b = int(input())
+        c = a/b
+        print(int(c))
+    elif(i == 5):
+        a = int(input())
+        b = int(input())
+        c = a//b
+        print(int(c))
+    elif(i == 6):
+        quit()
+    else:
+        print("Invalid Operation")
