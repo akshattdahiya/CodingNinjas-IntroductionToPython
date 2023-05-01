92aee8388cc20b05afeb18a2055b171469015035
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-01 21:23:04 +0530

    Create Number Pattern 1.py

---
+++ Milestone1/3. Patterns 1/Number Pattern 1.py
@@ -0,0 +1,13 @@
+from math import *
+from collections import *
+from sys import *
+from os import *
+n = int(input())
+i=1
+while i<=n:
+    j=1
+    while j<=i:
+        print('1' , end="")
+        j=j+1
+    print()
+    i=i+1
