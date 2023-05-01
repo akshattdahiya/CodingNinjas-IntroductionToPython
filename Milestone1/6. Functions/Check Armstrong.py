baf1b39e2505ce1d52fc5c9b4bd68fb3b681d7a3
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-01 21:50:03 +0530

    Create Check Armstrong.py

---
+++ Milestone1/6. Functions/Check Armstrong.py
@@ -0,0 +1,13 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+num = input()
+n = [int(i) for i in num]
+count = 0
+for j in range(len(n)):
+    count = count+n[j]**len(n)
+if count == int(num):
+    print("true")
+else:
+    print("false")
