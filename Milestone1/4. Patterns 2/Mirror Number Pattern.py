16a278cab5040b88cccf1b8d5ab6108686bfd8eb
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-01 21:27:34 +0530

    Create Mirror Number Pattern.py

---
+++ Milestone1/4. Patterns 2/Mirror Number Pattern.py
@@ -0,0 +1,13 @@
+n = int(input())
+i = 1
+while i<=n:
+    space=1
+    while space<=(n-i):
+        print(" " , end="")
+        space+=1
+    num = 1
+    while num<=i:
+        print(num , end="")
+        num+=1
+    print()
+    i+=1
