a7e87fba793d7d32b58e454180d7f1d33c3968c4
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-08 18:43:59 +0530

    Create Find Duplicate.py

---
+++ Milestone2/Arrays and Lists/Find Duplicate.py
@@ -0,0 +1,25 @@
+import sys
+
+def duplicateNumber(arr, n) :
+    for i in range(n - 1) :
+        for j in range((i + 1), n) :
+            if arr[i] == arr[j] :
+                return arr[i] 
+
+def takeInput() :
+    n = int(sys.stdin.readline().strip())
+
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, sys.stdin.readline().strip().split()))
+    return arr, n
+
+t = int(sys.stdin.readline().strip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    print(duplicateNumber(arr, n))
+    t -= 1
+    
