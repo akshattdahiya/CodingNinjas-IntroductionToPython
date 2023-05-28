e7db59c76f441e266ce7fa7faaf2e1661de8bb37
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-05-28 18:18:00 +0530

    Create Reverse Each Word.py

---
+++ Milestone2/4. Strings/Reverse Each Word.py
@@ -0,0 +1,10 @@
+def reverseword(s): 
+   
+   w = s.split(" ")        
+                           
+   nw= [i[::-1] for i in w]
+                          
+   ns = " ".join(nw)
+   return ns
+s = input()
+print(reverseword(s))
