c31edaf2a7cee8160ac58064698b9632dbf6a86c
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-06-22 09:34:35 +0530

    Create Print 2D Array.py

---
+++ Milestone2/Test 2/Print 2D Array.py
@@ -0,0 +1,9 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+n,m=map(int,input().split())
+for i in range(n):
+    a=list(map(int,input().split()))
+    for j in range(n-i):
+        print(*a)
